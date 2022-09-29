043983c0db4d409ffd5f099ecbb3d0014b1e30fc
ghintema <harm.intemann@gmail.com>
2022-09-29 16:18:31 -0400

    Ready for submitt
    Add plausibilisation check for name, card number and expir date.

--- index.js
+++ index.js
@@ -18,28 +18,58 @@ const inputCVC = document.getElementById('cvc');
 const outputError = document.getElementById('feedback');
 const outputAdvice = document.getElementById('advice');
 
-let everythingOk = true;
+const buttonConfirm = document.getElementById('confirm');
+const buttonContinue = document.getElementById('continue');
+const buttonClose = document.getElementById('close');
 
+// Defining some regex for plausibilisation //
+let regexSpecChar= /[\!\"\§\$\%\&\/\(\)\=\?\<\>\+\*\[\]\,\@]/;
+let regexNum = /[0-9]/ 
+let regexLetters = /[a-zA-Z]/;
 
 
 function inputCheck() {
 
-    nameCheck(inputName.value);
-    luhneCheck(inputNumber.value);
-    monthCheck(inputMonth.value);
-    yearCheck(inputYear.value);
-    cvcCheck(inputCVC.value);
- 
-
-
-
-    if (everythingOk) {
-        formArea.style.display = 'none';
-        successArea.style.display = 'block';
+    let nameCheckResult = nameCheck(inputName.value);
+    if (nameCheckResult) {
+        outputError.innerHTML = "your Name";
+        outputAdvice.innerHTML = nameCheckResult;
+        errorArea.style.display = 'block';
+        return;
     }
+
+    let numberCheckResult = numberCheck(inputNumber.value)
+    if (numberCheckResult) {
+        outputError.innerHTML = "your card number"
+        outputAdvice.innerHTML = numberCheckResult;
+        errorArea.style.display = 'block';
+        return;
+    }
+
+    let dateCheckResult = dateCheck(inputMonth.value, inputYear.value)
+    if (dateCheckResult) {
+        outputError.innerHTML = "the expire date"
+        outputAdvice.innerHTML = dateCheckResult;
+        errorArea.style.display = 'block';
+        return;
+    }
+
+    
+    formArea.style.display = 'none';
+    successArea.style.display = 'block';
 }
-
-
+ 
+function numberCheck(code) {
+    
+    if (regexSpecChar.test(code) || regexLetters.test(code)) { 
+        return 'Onyl numbers allowed. Please remove all special characters and letters.'
+    }
+    
+    if (!luhneCheck(code)) {
+        return 'The number is implausbible. Please check your number and try again.'
+    }
+    return false;
+}
 
 function luhneCheck(code) {
 
@@ -59,24 +89,59 @@ function luhneCheck(code) {
 }
 
 function nameCheck(name) {
-
-    let regexNum = new RegExp([0-9]) 
-    let regexSpecial = RegExp('.!@#$%^&*()_+-=')
-
-    if (regexNum.test(name)) {
-        return 'Keine zahlen erlaubt';
-    }
-
-    if (regexSpecial.test(name)) {
-        return 'Keine Sonderzeichen erlaubt';
+    console.log(name)
+    if (regexSpecChar.test(name) || regexNum.test(name)) {
+        return 'Onyl letters allowed. Please remove all special characters and numbers.'
     }
 
     let space = name.indexOf(' ')
-    .match(regex) zur Suche von Zahlen im Namen...
-    if (space >= 2 && name.length >= 5) {
-        return true
+    if (space <= 2 && name.length <= 4) {
+        return 'Please enter complete name as listed on the card.'
+    }
+    return false;
+
+}
+
+function dateCheck(month, year) {
+
+    if (month.length != 2  || 
+        year.length  != 2  || 
+        regexLetters.test(month) ||
+        regexSpecChar.test(month) ||
+        regexLetters.test(year) ||
+        regexSpecChar.test(year) ) {
+        return 'Please enter the expire date in the appropriate format.';
     }
 
-    //Suche ein Leerzeichen. 
-    // Prüfkriterium: index[leerzeichen] >=2 und name.length >= 5 und keinerlei Zahlen!!
+    if (parseInt(month) < 1 || parseInt(month) > 12) {
+        return 'Month musst be a number between 1 and 12.'
+    }
+
+    if (parseInt(year) < 22) {
+        return 'Your card has expired.'
+    }
+    return false;
+
+
 }
+
+
+function returnToForm() {
+    console.log('returnToForm is invoked')
+    errorArea.style.display = 'none';
+    successArea.style.display = 'none';
+    formArea.style.display = 'block';
+}
+
+function returnToEmptyForm() {
+    inputName.value = '';
+    inputNumber.value = '';
+    inputMonth.value = '';
+    inputYear.value = '';
+    inputCVC.value = '';
+    returnToForm();
+}
+
+buttonConfirm.onclick = inputCheck;
+buttonClose.onclick = returnToForm;
+buttonContinue.onclick = returnToEmptyForm;
